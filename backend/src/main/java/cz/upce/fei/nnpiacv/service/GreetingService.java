dc8e381ba80d45ccb7b8ab3f61ff3d21e2bb3812
Viacheslav Dmytreko <viacheslavdmytrenko03@gmail.com>
2025-02-10 15:40:02 +0100

    CV-01 - Implement Spring Boot backend

---
+++ backend/src/main/java/cz/upce/fei/nnpiacv/service/GreetingService.java
@@ -0,0 +1,11 @@
+package cz.upce.fei.nnpiacv.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    public String sayGreeting() {
+        return "<h1>Hello 5</h1>";
+    }
+}
